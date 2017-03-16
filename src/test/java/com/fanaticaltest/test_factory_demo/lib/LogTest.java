5378344dd37e7ef83072ceb57b3054d1fc6741e1
Ziridis <samy@kacem.net>
2017-03-16 08:28:00 +0100

    20170316.1
    Implement proxy for curl call

--- src/test/java/com/fanaticaltest/test_factory_demo/lib/LogTest.java
+++ src/test/java/com/fanaticaltest/test_factory_demo/lib/LogTest.java
@@ -1,6 +1,10 @@
 package com.fanaticaltest.test_factory_demo.lib;
 
 import java.io.BufferedReader;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
+import java.net.Proxy;
 import java.net.URL;
 import java.io.IOException;
 import java.net.URLConnection;
@@ -11,11 +15,33 @@ import org.apache.commons.codec.binary.Base64;
 
 public class LogTest {
 
+  private static Property prop = new Property();
+  private static int api_proxy_required = Integer.parseInt(prop.read("api_proxy_required"));
+  private static String api_proxy_url = prop.read("api_proxy_url");
+  private static int api_proxy_port = Integer.parseInt(prop.read("api_proxy_port"));
+  private static String api_proxy_user = prop.read("api_proxy_user");
+  private static String api_proxy_pass = prop.read("api_proxy_pass");
+
   public static String send(String request) throws IOException{
     URL url = new URL(request);
     URLConnection uc = url.openConnection();
 
+    if (api_proxy_required == 1){
+      Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(api_proxy_url, api_proxy_port));
+
+      Authenticator authenticator = new Authenticator() {
+        public PasswordAuthentication getPasswordAuthentication() {
+          return (new PasswordAuthentication(api_proxy_user,
+              api_proxy_pass.toCharArray()));
+        }
+      };
+      Authenticator.setDefault(authenticator);
+
+      uc = url.openConnection(proxy);
+    }
+
     uc.setRequestProperty("X-Requested-With", "Curl");
+
 
     StringBuilder html = new StringBuilder();
     BufferedReader input = null;
@@ -43,6 +69,20 @@ public class LogTest {
   public static String sendBasicAuth(String request, String username, String password) throws IOException{
     URL url = new URL(request);
     URLConnection uc = url.openConnection();
+
+    if (api_proxy_required == 1){
+      Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(api_proxy_url, api_proxy_port));
+
+      Authenticator authenticator = new Authenticator() {
+        public PasswordAuthentication getPasswordAuthentication() {
+          return (new PasswordAuthentication(api_proxy_user,
+              api_proxy_pass.toCharArray()));
+        }
+      };
+      Authenticator.setDefault(authenticator);
+
+      uc = url.openConnection(proxy);
+    }
 
     uc.setRequestProperty("X-Requested-With", "Curl");
 
