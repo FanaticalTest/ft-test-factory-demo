668ceba6f2f27799d5ff75e4ee50ab196c914a40
Ziridis <samy@kacem.net>
2017-07-19 17:55:08 +0200

    20170719.1

--- src/test/java/com/fanaticaltest/test_factory_demo/CukesRunner.java
+++ src/test/java/com/fanaticaltest/test_factory_demo/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = {"src/test/resources"}
+    features = {"src/test/resources"},
+    plugin = {"pretty", "json:target/cucumber.json"}
 )
 public class CukesRunner {
 }
